9cf6f2ae4b9917ea8d95015aaaec475a122de717
4148 <angus@angusshire.com>
2015-08-17 04:07:47 -0700

    took way to long

--- check.py
+++ check.py
@@ -14,11 +14,8 @@ files = os.listdir(os.curdir)
 for f in files:
     if (".py" in f and "check" not in f and "~" not in f):
         sol_nums[int(f[:-3])] = is_empty(f)
-
-print sol_nums
-        #sol_nums_lst = list(sol_nums.keys()
-#print "ALL problems between", min(sol_nums), "and", max(sol_nums), "done EXCEP#T" 
-#for i in range(min(sol_nums), max(sol_nums)):
-#    if i not in sol_nums:
-#        print "\t", i
+print "ALL problems between", min(sol_nums), "and", max(sol_nums), "done EXCEPT" 
+for i in range(min(sol_nums), max(sol_nums)+1):
+    if i not in sol_nums or sol_nums[i]:
+        print "\t", i
 print "Done"
